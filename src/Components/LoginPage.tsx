b8c2c37e8f1409f2162f2c541f8145dc24316619
Ahmed-Jan-Farooqui <aahmaad.jan@gmail.com>
2024-02-24 20:56:40 +0500

    added tailwind

--- src/Components/LoginPage.tsx
+++ src/Components/LoginPage.tsx
@@ -9,6 +9,8 @@ function loginPage() {
       <div className="right-div">
         <div className="login-div">
           <p>My login goes here.</p>
+          <button className="login-button">Login</button>
+          <button className="signup-button">Sign Up</button>
         </div>
       </div>
     </div>
