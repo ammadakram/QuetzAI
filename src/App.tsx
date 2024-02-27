a42a3c5b5e2553265655b941b0683980e420fc85
LOLEDAKAASI <25100116@lums.edu.pk>
2024-02-27 19:21:18 +0500

    added a password page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import SignupPage from './Components/SignupPage';
+import SignupPagePassword from './Components/SingupPagePassword';
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<SignupPage />} />
+          <Route path="/" element={<SignupPagePassword />} />
         </Routes>
       </Router>
     </div>
