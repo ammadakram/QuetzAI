5eddddce3b3f76496263a0ffa9b4b3fa5399224d
Ahmed-Jan-Farooqui <aahmaad.jan@gmail.com>
2024-03-03 23:06:38 +0500

    Set up routes. Set up the sign up via email and password logic using Firebase APIs. Added conditional rendering.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import LogInPage from "./Components/LogInPage";
 import LogInPassword from "./Components/LogInPassword";
 import LandingPage from "./Components/LandingPage1";
+import SignupPage from "./Components/SignupPage";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
+          <Route path="/signup" element={<SignupPage />} />
           <Route path="/" element={<LandingPage />} />{" "}
           {/* Need to route this differently later. We will be checking if user exists when they log on. */}
           <Route path="/login" element={<LogInPage />} />
